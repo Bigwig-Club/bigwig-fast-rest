55b173b065404af853f354b6fe6f7fecfa776c88
m01i0ng <alley.ma@qq.com>
2021-01-04 15:45:53 +0800

    feat: 添加 jackson 时间序列化模块配置

--- src/main/java/dev/bigwig/fastrest/common/config/WebConfig.java
+++ src/main/java/dev/bigwig/fastrest/common/config/WebConfig.java
@@ -4,8 +4,19 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.Formatter;
 
 @Configuration
 public class WebConfig {
@@ -14,7 +25,17 @@ public class WebConfig {
   public ObjectMapper objectMapper() {
     ObjectMapper mapper = new ObjectMapper();
     mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-    mapper.findAndRegisterModules();
+
+    mapper.registerModule(new Hibernate5Module());
+
+    JavaTimeModule javaTimeModule = new JavaTimeModule();
+    javaTimeModule.addSerializer(LocalDateTime.class,
+        new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    javaTimeModule.addSerializer(LocalDate.class,
+        new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    javaTimeModule.addSerializer(LocalTime.class,
+        new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+    mapper.registerModule(javaTimeModule);
 
     mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -22,5 +43,50 @@ public class WebConfig {
     mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
 
     return mapper;
+  }
+
+  @Bean
+  public Formatter<LocalDate> localDateFormatter() {
+    return new Formatter<>() {
+      @Override
+      public String print(LocalDate object, Locale locale) {
+        return object.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+      }
+
+      @Override
+      public LocalDate parse(String text, Locale locale) {
+        return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+      }
+    };
+  }
+
+  @Bean
+  public Formatter<LocalTime> localTimeFormatter() {
+    return new Formatter<>() {
+      @Override
+      public String print(LocalTime object, Locale locale) {
+        return object.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+      }
+
+      @Override
+      public LocalTime parse(String text, Locale locale) {
+        return LocalTime.parse(text, DateTimeFormatter.ofPattern("HH:mm:ss"));
+      }
+    };
+  }
+
+  @Bean
+  public Formatter<LocalDateTime> localDateTimeFormatter() {
+    return new Formatter<>() {
+      @Override
+      public String print(LocalDateTime object, Locale locale) {
+        return object.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+      }
+
+      @Override
+      public LocalDateTime parse(String text, Locale locale) {
+        return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+      }
+    };
   }
 }

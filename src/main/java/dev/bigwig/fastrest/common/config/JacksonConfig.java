afb214d5a3e2f3822b96885aa6b1cee46eedc7a2
m01i0ng <alley.ma@qq.com>
2021-01-04 16:02:49 +0800

    refactor: rename WebConfig to JacksonConfig

--- src/main/java/dev/bigwig/fastrest/common/config/JacksonConfig.java
+++ src/main/java/dev/bigwig/fastrest/common/config/JacksonConfig.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.Formatter;
 
 @Configuration
-public class WebConfig {
+public class JacksonConfig {
 
   @Bean
   public ObjectMapper objectMapper() {
